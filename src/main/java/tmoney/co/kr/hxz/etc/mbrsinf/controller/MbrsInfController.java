a74ab97db5353f5e25e107507a4d235887b8f0bf
harrysci <harrysci@naver.com>
2025-10-30 10:21:05 +0900

    회원정보 폴더 수정

--- src/main/java/tmoney/co/kr/hxz/etc/mbrsinf/controller/MbrsInfController.java
+++ src/main/java/tmoney/co/kr/hxz/etc/mbrsinf/controller/MbrsInfController.java
@@ -35,7 +35,70 @@ public class MbrsInfController {
     ) {
         MbrsInfRspVO mbrsInf = mbrsInfService.readMbrsInf(mbrsId);
         model.addAttribute("mbrsInf", mbrsInf);
-        return "/hxz/mypage/mbrsinf/mbrsInf";
+        return "/hxz/etc/mbrsinf/mbrsInf";
+    }
+
+    /**
+     * 회원 정보 수정
+     *
+     * tbhxzm101 HXZ_회원정보
+     * tbhxzh101 HXZ_회원정보변경이력
+     *
+     * [process]
+     * 1. HXZ_회원정보 테이블 내의 회원 정보 수정
+     * 2. HXZ_회원정보변경이력 테이블 회원정보 변경 이력 추가
+     *
+     * @param req
+     * @return
+     */
+    @PostMapping("/")
+    @ResponseBody
+    public ResponseEntity<?> updateMbrsInf(
+            @Valid @RequestBody MbrsUpdReqVO req
+    ) {
+        mbrsInfService.updateMbrsInf(req);
+        return ResponseEntity.ok().build();
+    }
+
+    /**
+     * 비밀번호 변경 페이지
+     *
+     * @return
+     */
+    @GetMapping("/{mbrsId}/pwdForm.do")
+    public String pwdForm(
+            @PathVariable("mbrsId") String mbrsId,
+            Model model
+    ) {
+        MbrsInfRspVO mbrsInf = mbrsInfService.readMbrsInf(mbrsId);
+
+        model.addAttribute("mbrsInf", mbrsInf);
+        model.addAttribute("mbrsId", mbrsId);
+        return "/hxz/etc/mbrsinf/pwdForm";
+    }
+
+    /**
+     * 비밀번호 변경 처리
+     */
+    @PostMapping("/pwd/{mbrsId}")
+    @ResponseBody
+    public ResponseEntity<?> updatePwd(
+            @RequestParam String newPwd,
+            @RequestParam String cfmPwd,
+            @PathVariable("mbrsId") String mbrsId
+    ) {
+        mbrsInfService.updatePwd(mbrsId, newPwd, cfmPwd);
+        return ResponseEntity.ok().build();
+    }
+
+    /**
+     * 회원 탈퇴 페이지
+     *
+     * @return
+     */
+    @GetMapping("/mbrsScsn.do")
+    public String mbrsScsn() {
+        return "/hxz/etc/mbrsinf/mbrsScsn";
     }
 }
 
